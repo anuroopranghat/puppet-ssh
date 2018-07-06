c2e19d7e7a411f74f11828a1d52716fea229261f
root <root@anuroopranghat1.mylabserver.com>
2018-07-06 04:09:35 +0000

    Updated

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,5 +1,9 @@
-class ssh::install {
-  package {'openssh-server':
-    ensure =>present,
+class ssh::install(
+  String $package_name = $::ssh::package_name,
+  String $service_name = $::ssh::service_name,
+) {
+  package {'ssh-package':
+    ensure => present,
+    name   => $package_name,
   }
 }
